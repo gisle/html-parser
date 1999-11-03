389c7992ee705afb636a7ff30d7dfe1dab8edbe3
gisle <gisle>
1999-11-03 14:13:06 +0000

    Added copyright notice.

--- c/Parser.pm
+++ c/Parser.pm
@@ -1,9 +1,14 @@
 package HTML::Parser;
 
+# Copyright 1996-1999, Gisle Aas.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the same terms as Perl itself.
+
 use strict;
 use vars qw($VERSION @ISA);
 
-$VERSION = '2.99_01';  # $Date: 1999/11/03 12:17:09 $
+$VERSION = '2.99_01';  # $Date: 1999/11/03 14:13:06 $
 
 require DynaLoader;
 @ISA=qw(DynaLoader);
