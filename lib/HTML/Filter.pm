298129986e5029213dfabe4bdb0fba13309bf681
gisle <gisle>
1999-11-10 13:13:07 +0000

    Rely on the complete process instructions to be available is second argument.  Without this we would need special stuff in xml_mode.

--- lib/HTML/Filter.pm
+++ lib/HTML/Filter.pm
@@ -3,10 +3,10 @@ package HTML::Filter;
 require HTML::Parser;
 @ISA=qw(HTML::Parser);
 
-$VERSION = sprintf("%d.%02d", q$Revision: 2.6 $ =~ /(\d+)\.(\d+)/);
+$VERSION = sprintf("%d.%02d", q$Revision: 2.7 $ =~ /(\d+)\.(\d+)/);
 
 sub declaration { $_[0]->output("<!$_[1]>")     }
-sub process     { $_[0]->output("<?$_[1]>")     }
+sub process     { $_[0]->output($_[2])          }
 sub comment     { $_[0]->output("<!--$_[1]-->") }
 sub start       { $_[0]->output($_[4])          }
 sub end         { $_[0]->output($_[2])          }
