914e182e471c725c51b471655c2c67f7a7b59fd2
gisle <gisle>
1999-11-18 08:19:25 +0000

    Callbacks are now set up with _cb suffix.

--- lib/HTML/LinkExtor.pm
+++ lib/HTML/LinkExtor.pm
@@ -25,7 +25,7 @@ parser by calling the $p->parse() or $p->parse_file() methods.
 
 require HTML::Parser;
 @ISA = qw(HTML::Parser);
-$VERSION = sprintf("%d.%02d", q$Revision: 1.24 $ =~ /(\d+)\.(\d+)/);
+$VERSION = sprintf("%d.%02d", q$Revision: 1.25 $ =~ /(\d+)\.(\d+)/);
 
 use strict;
 use vars qw(%LINK_ELEMENT);
@@ -83,7 +83,7 @@ non-link attributes are removed.
 sub new
 {
     my($class, $cb, $base) = @_;
-    my $self = $class->SUPER::new(start => \&_start_tag,
+    my $self = $class->SUPER::new(start_cb => \&_start_tag,
 				  pass_cbdata => 1,
 				 );
     $self->{extractlink_cb} = $cb;
