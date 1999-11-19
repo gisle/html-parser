ec9f035764cde41f10708dd5b1e5a74c8d063c8f
gisle <gisle>
1999-11-19 13:04:35 +0000

    Various documentation updates.

--- Parser.pm
+++ Parser.pm
@@ -8,7 +8,7 @@ package HTML::Parser;
 use strict;
 use vars qw($VERSION @ISA);
 
-$VERSION = 2.99_13;  # $Date: 1999/11/19 11:04:10 $
+$VERSION = 2.99_13;  # $Date: 1999/11/19 13:04:35 $
 
 require HTML::Entities;
 
@@ -117,7 +117,7 @@ HTML::Parser - HTML tokenizer
 =head1 SYNOPSIS
 
  require HTML::Parser;
- $p = HTML::Parser->new;  # should really a be subclass
+ $p = HTML::Parser->new( %options );
  $p->parse($chunk1);
  $p->parse($chunk2);
  #...
@@ -129,26 +129,38 @@ HTML::Parser - HTML tokenizer
  open(F, "foo.html") || die;
  $p->parse_file(*F);
 
-=head1 DESCRIPTION
+=head1 NOTE
 
-B<Note: > This is the new experimental XS based HTML::Parser.  It
-should be completely backwards compatible with HTML::Parser version
-2.2x, but has many new features (not documented yet).  The rest of
-this manual page describes HTML::Parser v2.25.
+This is the new experimental XS based HTML::Parser.  It should be
+completely backwards compatible with HTML::Parser version 2.2x, but
+has many new features.
+
+=head1 DESCRIPTION
 
 The C<HTML::Parser> will tokenize an HTML document when the parse() or
 parse_file() methods are called.  Tokens are reported by invoking
-various callback methods.  The document to be parsed can be supplied
-in arbitrary chunks.
+various callback methods or by accumulating tokens in an array.  The
+document to be parsed can be supplied in arbitrary chunks.
 
-=head2 External interface Methods
+[XXX Some more general talk...]
 
-=item $p = HTML::Parser->new( 'handlers' => { event => \&subroutine, ... } )
+=head1 METHODS
+
+=over
+
+=item $p = HTML::Parser->new( %options_and_callbacks )
 
 The object constructor creates a HTML::Parser object
 and may assign callback subroutines for various events.
 
-=item $p->parse( $string );
+[XXX describe how %options_and_callbacks maps to the callback method
+and the various boolean options.]
+
+For compatibility with HTML::Parser version 2 the constructor will set
+up method callbacks and turn on the $p->v2_compat option if no
+%options_and_callbacks are given.
+
+=item $p->parse( $string )
 
 Parse $string as the next chunk of the HTML document.  The return
 value is a reference to the parser object (i.e. $p).
@@ -159,7 +171,7 @@ Signals the end of the HTML document.  Calling the eof() method will
 flush any remaining buffered text.  The return value is a reference to
 the parser object.
 
-=item $p->parse_file( $file );
+=item $p->parse_file( $file )
 
 This method can be called to parse text directly from a file.  The
 $file argument can be a filename or an already opened file handle (or
@@ -173,141 +185,101 @@ the parser object.
 If a filehandle is passed in, then the file will be read until EOF,
 but not otherwise affected.
 
-=head2 Callback Subroutines
-
-Callback subroutines may be assigned to events as arguments to
-the constructor or by calling $p->callback.
-Assigned callbacks override the corresponding subclass methods.
-
-If $p->pass_self is enabled, $p will be passed ahead of the other arguments.
-
 =item $p->callback( event => \&subroutine )
 
-This method assigns a subroutine (which may be an anonymous subroutine)
-as a callback for an event.
+This method assigns a subroutine (which may be an anonymous
+subroutine) as a callback for an event.  Event is one of C<text>,
+C<start>, C<end>, C<declaration>, C<comment>, C<process> or
+C<default>.  Look at L</HANDLERS> for details on arguments passed to
+the callbacks.
 
-=over 4
+=item $p->bool_attr_value( $val )
 
-=item text_cb => \&text( $text )
+This method sets up the value reported for boolean attributes inside
+HTML start tags.  By default the name of the attribute is also used as
+its value.
 
-This event is triggered when plain text in the document is recognized.
-The text is passed unmodified and might contain multiple lines.
-Note that for efficiency reasons entities in the text are B<not>
-expanded unless $p->decode_text_entities is enabled.
-If it isn't, you should call HTML::Entities::decode($text) before you
-process the text any further.
+=item $p->accum( $array_ref )
 
-A sequence of text in the HTML document may be broken between several
-invocations of $self->text unless $p->unbroken_text is enabled.
+This method tell the parser to not make callbacks but instead append
+entries to the array given as tokens are recognized.
 
-The parser will make sure that it does not break a word or
-a sequence of spaces between two events.
-
-
-=item start_cb => \&start( $tag, $attr, $attrseq, $origtext )
-
-This event is triggered when a complete start tag has been recognized.
-The first argument is the tag name (in lower case) and the second
-argument is a reference to a hash that contain all attributes found
-within the start tag.  The attribute keys are converted to lower case.
-Entities found in the attribute values are already expanded.  The
-third argument is a reference to an array with the lower case
-attribute keys in the original order.  The fourth argument is the
-original HTML text.
-
-
-
-=item end_cb => \&end( $tag, $origtext )
-
-This event is triggered when an end tag has been recognized.
-The first argument is the lower case tag name, the second the original
-HTML text of the tag.
-
-
-=item decl_cb => \&declaration( $string )
-
-This event is triggered when a I<markup declaration> has been recognized.
-The only argument is the declaration text.
-Comments and the surrounding "<!" and ">" are removed.
-Entities are B<not> be expanded.
-
-For typical HTML documents, the only declaration you are
-likely to find is <!DOCTYPE ...>.
-
-
-=item com_cb => \&comment( $comment )
-
-This event is triggered when a comment is recognized.
-The leading and trailing "--" sequences have been stripped
-from the comment text.
-
-
-=item pi_cb
-
-
-
-=item default_cb
-
+[XXX Describe how tokens are reported (similar to HTML::TokeParser)]
 
 =back
 
-=head2 Boolean Attribute Accessor Methods
 
-Each attribue is enabled by calling the corresponding method
-with a TRUE argument and disabled with a FALSE argument.
+=head1 PARSER OPTIONS
 
-The return value from each method is the old attribute value.
+Parser options are represented by boolean parser attributes.  Each
+attribute is enabled by calling the corresponding method with a TRUE
+argument and disabled with a FALSE argument.  The attribute value is
+left unchanged if no argument is given.  The return value from each
+method is the old attribute value.
+
+The boolean parser option methods are:
+
+=over
 
 =item $p->strict_comment( [$bool] )
 
 By default, comments are terminated by the first occurrence of "-->".
-This is close to the behaviour of some popular browsers
-(like Netscape and MSIE),
-but it is not correct according to the "official" HTML standards.
+This is the behaviour of most popular browsers (like Netscape and
+MSIE), but it is not correct according to the "official" HTML
+standards.
 
 The official behaviour is enabled by enabling this attribute.
 
 =item $p->strict_names( [$bool] )
 
 By default, almost anything is allowed in tag and attribute names.
-This is close to the behaviour of some popular browsers and allows us
-to parse some broken tags with invalid values in the attr values like:
-   <IMG ALIGN=MIDDLE SRC=newprevlstGr.gif ALT=[PREV LIST] BORDER=0> 
+This is the behaviour of most popular browsers and allows us to parse
+some broken tags with invalid values in the attr values like:
 
-The official behaviour is enabled by enabling this attribute.
+   <IMG SRC=newprevlstGr.gif ALT=[PREV LIST] BORDER=0>
+
+The official behaviour is enabled by enabling this attribute.  This
+will make a tag like the one above be parsed as text instead.
 
 =item $p->decode_text_entities( [$bool] )
 
-When this attribute is enabled, HTML entities passed to $self->text()
-or the text callback (&amp;, &#256;, ...) are automatically decoded.
+When this attribute is enabled, HTML entities (&amp;, &#255;, ...) are
+automatically decoded in the text reported.
 
 =item $p->keep_case( [$bool] )
 
-By default, tag and attr names are forced to lower case.
-When this attribute is enabled, the original text of the names is preserved.
+By default, tag and attr names are forced to lower case.  When this
+attribute is enabled, the original text of the names is preserved.
 
-Enabling xml_mode also causes this behaviour without changing this attribute.
+Enabling xml_mode also causes this behaviour even if the keep_case
+value remain FALSE.
 
 =item $p->xml_mode( [$bool] )
 
-Enabling this attribute changes the parser to allow XML tags.
-It also disables forcing tag and attr names to lower case,
-changes the way that empty tags are handled,
-and allows '/' as the last character inside a start tag.
+Enabling this attribute changes the parser to allow some XML tags.  It
+also disables forcing tag and attr names to lower case.
 
-An empty XML tag causes an artificial end tag to be generated.
+Empty element tags are like start tags, but ends with the characters
+"/>".  When recognized by HTML::Parser these will causes an artificial
+end tag to be generated in addition to the start tag.
+
+Processing instructions are terminated by "?>" instead of simply ">".
 
 =item $p->v2_compat( [$bool] )
 
-Enabling this attribute causes start parameter reporting to be
-closer to the behaviour of HTML::Parser v2.
-
-XXX What is the difference?
+Enabling this attribute causes start parameter reporting to be closer
+to the behaviour of HTML::Parser v2.  More information in L</VERSION 2
+COMPATIBILITY>.
 
 =item $p->pass_self( [$bool] )
 
-Enabling this attribute causes $self to be the first argument
-to callback subroutines so they can act like method calls.
+Enabling this attribute causes $self to be the first argument to
+callback subroutines.  Useful if the callback wants to access
+attributes of the parser object or call methods on it.
+
+(If register closures with references to the parser as handlers, then
+you effectively create circular sturctures that prevent the parser
+from being garbarge collected.)
 
 XXX Should this be implied by v2_compat?
 
@@ -318,7 +290,7 @@ which may be of any length, but won't end inside a word.
 When this attribute is enabled
 blocks of text are always returned in one piece.
 
-Text in cdata sections may still be broken up.
+Text in cdata sections may still be broken up.  [XXX Wrong!]
 
 =item $p->attr_pos( [$bool] )
 
@@ -329,94 +301,157 @@ When this attribute is enabled a reference to an array
 is substituted for the attr value.
 
 The array elements are the offsets in the tag text of
-(1) the end of the previous attr value or name,
-(2) the start of the attr name,
-(3) the start of the attr value (undef if no value), and
-(4) the end of the attr value (name if no value).
 
-In this case, the attr value will not have HTML entities decoded.
+ (0) the end of the previous attr value or name,
+ (1) the start of the attr name,
+ (2) the start of the attr value (undef if no value), and
+ (3) the end of the attr value (name if no value).
+
+Examples of use:
+
+ $pos = $attr->{bgcolor};
+ if ($pos) {
+    # kill any bgclor attributes
+    substr($origtext, $pos->[0], $pos->[3] - $pos->[0]) = "";
+
+    # set value to yellow
+    substr($origtext, $pos->[2], pos->[3] - $pos->[2]) = "yellow";
+
+    # update attribute name
+    substr($origtext, $pos->[1], length("bgcolor") = "x-bgcolor";
+ }
+
 
 =item $p->marked_section( [$bool] )
 
 B<Note:> This attribute is only available
 if the Marked Section option was selected at compile time.
 
-By default section markings like CDATA[] are treated like ordinary text.
-When this attribute is enabled section markings are honored.
+By default section markings like <![CDATA[...]]> are treated like
+ordinary text.  When this attribute is enabled section markings are
+honored.
 
 XXX I don't really know what this is about.
 
-=head2 Subclassing
+http://www.sgml.u-net.com/book/sgml-8.htm
 
-The default implementation of these methods do nothing, i.e., the
-tokens are just ignored.
-This was the only way to modify the behaviour of HTML::Parser v2.
-
-In order to make the parser do anything interesting, you must make a
-subclass where you override one or more of the following methods as
-appropriate or assign callback subroutines.
+=back
 
 
-=item $self->declaration($decl)
 
-This method is called when a I<markup declaration> has been
-recognized.  For typical HTML documents, the only declaration you are
-likely to find is <!DOCTYPE ...>.  The initial "<!" and ending ">" is
-not part of the string passed as argument.  Comments are removed and
-entities will B<not> be expanded.
+=head1 HANDLERS
 
-=item $self->start($tag, $attr, $attrseq, $origtext)
+Callback subroutines may be assigned to events as arguments to
+the constructor or by calling $p->callback.
+Assigned callbacks override the corresponding subclass methods.
 
-This method is called when a complete start tag has been recognized.
+If $p->pass_self is enabled, $p will be passed ahead of the other
+arguments.
+
+=over
+
+=item text( $text, $cdata_flag )
+
+This event is triggered when plain text in the document is recognized.
+The text might contain multiple lines.  A sequence of text in the HTML
+document may be broken between several invocations of the text handler
+unless $p->unbroken_text is enabled.
+
+The parser will make sure that it does not break a word or
+a sequence of spaces between two events.
+
+The $cdata_flag is TRUE if $text might contain entity references that
+should be expanded.  If $p->decode_text_entities is enabled, then the
+$cdata_flag will always be TRUE.  If $p->decode_text_entities is
+disabled, then this flag will only be true for text found inside CDATA
+elements, like <script>, <style>, <xmp> and for CDATA marked sections.
+
+When $cdata_flag is FALSE (or missing) then you might want to call
+HTML::Entities::decode($text) before you process the text any further.
+
+
+=item start( $tag, $attr, $origtext )
+
+This event is triggered when a complete start tag has been recognized.
 The first argument is the tag name (in lower case) and the second
-argument is a reference to a hash that contain all attributes found
-within the start tag.  The attribute keys are converted to lower case.
-Entities found in the attribute values are already expanded.  The
-third argument is a reference to an array with the lower case
-attribute keys in the original order.  The fourth argument is the
-original HTML text.
+argument is a reference to an array that contain all attributes found
+within the start tag.  Attributes are prepresented by key/value pairs.
+The attribute keys are converted to lower case.  Any entities found in
+the attribute values are already expanded.
 
+The third argument is the original HTML text.
 
-=item $self->end($tag, $origtext)
+=item end( $tag, $origtext )
 
-This method is called when an end tag has been recognized.  The
-first argument is the lower case tag name, the second the original
+This event is triggered when an end tag has been recognized.
+The first argument is the lower case tag name, the second the original
 HTML text of the tag.
 
-=item $self->text($text)
 
-This method is called when plain text in the document is recognized.
-The text is passed on unmodified and might contain multiple lines.
-Note that for efficiency reasons entities in the text are B<not>
-expanded unless $p->decode_text_entities is enabled.
-If it isn't, you should call HTML::Entities::decode($text) before you
-process the text any further.
+=item declaration( $tokens, $origtext )
 
-A sequence of text in the HTML document can be broken between several
-invocations of $self->text.  The parser will make sure that it does
-not break a word or a sequence of spaces between two invocations of
-$self->text().
+This event is triggered when a I<markup declaration> has been recognized.
 
-=item $self->comment($comment)
+For typical HTML documents, the only declaration you are
+likely to find is <!DOCTYPE ...>.
 
-This method is called as comments are recognized.  The leading and
-trailing "--" sequences have been stripped off the comment text.
 
-=head2 SGML
+=item comment( $comment )
 
-There is really nothing in the basic parser that is HTML specific, so
-it is likely that the parser can parse other kinds of SGML documents.
-SGML has many obscure features (not implemented by this module) that
-prevent us from renaming this module as C<SGML::Parser>.
+This event is triggered when a comment is recognized.
+The leading and trailing "--" sequences have been stripped
+from the comment text.
 
-=head1 EFFICIENCY
+[XXX: Should there be an $origtext?  One problem is that if you turn
+on $p->strict_comment, then a comment like this one <!-- foo -- -- bar
+--> will be reported as two comments, and then $origtext does not make
+sense.]
 
-The parser is fairly inefficient if the chunks passed to $p->parse()
-are too big.  The reason is probably that perl ends up with a lot of
-character copying as tokens are chopped of from the beginning of the
-strings.  A chunk size of about 256-512 bytes was optimal in a test I
-made with some real world HTML documents.  (The parser was about 3
-times slower with a chunk size of 20K).
+
+=item process( $content, $origtext )
+
+This event is triggered when a process instruction is recognized.
+
+<http://www.sgml.u-net.com/book/sgml-8.htm>
+
+=item default( $origtext )
+
+This event is triggered for anything that does not have a specific
+callback.
+
+Example that strips out <font> tags:
+
+  sub ignore_font { print pop unless shift eq "font" }
+  HTML::Parser->new(default_cb => sub { print shift },
+                    start_cb => \&ignore_font,
+                    end_cb => \&ignore_font,
+                   )->parse_file(shift)
+
+Example that strips comments:
+
+  HTML::Parser->new(default_cb => sub { print shift },
+                    comment_cb => sub { },
+                   )->parse_file(shift);
+
+=back
+
+=head1 VERSION 2 COMPATIBILITY
+
+[XXX Describe callback methods in general and how they map to the handlers
+described above.]
+
+[XXX Describe how the arguments for the start callback method differ
+from what is described for the start handler above.  [Attributes are
+reported as a hash and an additional \@attr_seq argument is inserted]
+
+[XXX Describe the effect of $p->v2_compat.]
+
+
+=head1 EXAMPLES
+
+[XXX I want this to be a HTML::Parser cookbook.  Also show how we
+simplify all the HTML recipes found in the "Perl Cookbook" with the
+new features.]
 
 =head1 SEE ALSO
 
