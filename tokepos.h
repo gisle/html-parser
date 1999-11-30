a9ea92bd872aa1991674050b119f49f5265186e1
gisle <gisle>
1999-11-30 04:38:44 +0000

    First revision.

---
+++ tokepos.h
@@ -0,0 +1,44 @@
+struct token_pos
+{
+  char *beg;
+  char *end;
+};
+typedef token_pos token_pos_t;
+
+#define dTOKENS(init_lim) \
+   token_pos_t token_buf[init_lim]; \
+   int token_lim = init_lim; \
+   token_pos_t *tokens = token_buf; \
+   int num_tokens = 0
+
+#define PUSH_TOKEN(p_beg, p_end) \
+   STMT_START {
+     if (++num_tokens == token_lim) \
+       tokens_grow(*tokens, *token_lim, (tokens != token_buf)); \
+     tokens[num_tokens].beg = p_beg; \
+     tokens[num_tokens].end = p_end; \
+   } STMT_END
+
+#define FREE_TOKENS \
+   STMT_START {
+     if (tokens != token_buf) \
+       Safefree(tokens);
+   } STMT_END
+
+static void
+tokens_grow(token_pos_t **token_ptr, int *token_lim_ptr, bool realloc)
+{
+  int new_lim = *token_lim_ptr * 2;
+  if (realloc) {
+    Renew(*token_ptr, new_lim, struct token_pos);
+  }
+  else {
+    struct token_pos *new_tokens;
+    int i;
+    New(57, new_tokens, new_lim, struct token_pos);
+    for (i = 0; i < *token_lim_ptr; i++)
+      new_tokens[i] = token_ptr->[i];
+    *token_ptr = new_tokens;
+  }
+  *token_lim_ptr = new_lim;
+}
