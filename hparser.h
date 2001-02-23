532158b542cf201391f93f9e356b044980b95d2c
gisle <gisle>
2001-02-23 07:08:01 +0000

    Reindent.

--- hparser.h
+++ hparser.h
@@ -1,4 +1,4 @@
-/* $Id: hparser.h,v 2.11 2000/03/20 11:55:08 gisle Exp $
+/* $Id: hparser.h,v 2.12 2001/02/23 07:08:01 gisle Exp $
  *
  * Copyright 1999-2000, Gisle Aas
  * Copyright 1999-2000, Michael A. Chase
@@ -16,11 +16,11 @@
 #ifdef MARKED_SECTION
 
 enum marked_section_t {
-  MS_NONE = 0,
-  MS_INCLUDE,
-  MS_RCDATA,
-  MS_CDATA,
-  MS_IGNORE
+    MS_NONE = 0,
+    MS_INCLUDE,
+    MS_RCDATA,
+    MS_CDATA,
+    MS_IGNORE
 };
 
 #endif /* MARKED_SECTION */
@@ -29,66 +29,66 @@ enum marked_section_t {
 #define P_SIGNATURE 0x16091964  /* tag struct p_state for safer cast */
 
 enum event_id {
-  E_DECLARATION = 0,
-  E_COMMENT,
-  E_START,
-  E_END,
-  E_TEXT,
-  E_PROCESS,
-  E_DEFAULT,
-  /**/
-  EVENT_COUNT
+    E_DECLARATION = 0,
+    E_COMMENT,
+    E_START,
+    E_END,
+    E_TEXT,
+    E_PROCESS,
+    E_DEFAULT,
+    /**/
+    EVENT_COUNT
 };
 typedef enum event_id event_id_t;
 
 /* must match event_id_t above */
 static char* event_id_str[] = {
-  "declaration",
-  "comment",
-  "start",
-  "end",
-  "text",
-  "process",
-  "default",
+    "declaration",
+    "comment",
+    "start",
+    "end",
+    "text",
+    "process",
+    "default",
 };
 
 struct p_handler {
-  SV* cb;
-  SV* argspec;
+    SV* cb;
+    SV* argspec;
 };
 
 struct p_state {
-  U32 signature;
-  SV* buf;
-  STRLEN chunk_offset;
-  bool parsing;
-  bool eof;
+    U32 signature;
+    SV* buf;
+    STRLEN chunk_offset;
+    bool parsing;
+    bool eof;
 
-  /* special parsing modes */
-  char* literal_mode;
-  bool  is_cdata;
+    /* special parsing modes */
+    char* literal_mode;
+    bool  is_cdata;
 
-  /* unbroken_text option needs a buffer of pending text */
-  SV*    pend_text;
-  bool   pend_text_is_cdata;
-  STRLEN pend_text_offset;
+    /* unbroken_text option needs a buffer of pending text */
+    SV*    pend_text;
+    bool   pend_text_is_cdata;
+    STRLEN pend_text_offset;
 
 #ifdef MARKED_SECTION
-  /* marked section support */
-  enum marked_section_t ms;
-  AV* ms_stack;
-  bool marked_sections;
+    /* marked section support */
+    enum marked_section_t ms;
+    AV* ms_stack;
+    bool marked_sections;
 #endif
 
-  /* various boolean configuration attributes */
-  bool strict_comment;
-  bool strict_names;
-  bool xml_mode;
-  bool unbroken_text;
+    /* various boolean configuration attributes */
+    bool strict_comment;
+    bool strict_names;
+    bool xml_mode;
+    bool unbroken_text;
 
-  /* other configuration stuff */
-  SV* bool_attr_val;
-  struct p_handler handlers[EVENT_COUNT];
+    /* other configuration stuff */
+    SV* bool_attr_val;
+    struct p_handler handlers[EVENT_COUNT];
 };
 typedef struct p_state PSTATE;
 
--- tokenpos.h
+++ tokenpos.h
@@ -1,7 +1,7 @@
 struct token_pos
 {
-  char *beg;
-  char *end;
+    char *beg;
+    char *end;
 };
 typedef struct token_pos token_pos_t;
 
@@ -13,37 +13,37 @@ typedef struct token_pos token_pos_t;
 
 #define PUSH_TOKEN(p_beg, p_end) \
    STMT_START { \
-     ++num_tokens; \
-     if (num_tokens == token_lim) \
-       tokens_grow(&tokens, &token_lim, (tokens != token_buf)); \
-     tokens[num_tokens-1].beg = p_beg; \
-     tokens[num_tokens-1].end = p_end; \
+       ++num_tokens; \
+       if (num_tokens == token_lim) \
+           tokens_grow(&tokens, &token_lim, (tokens != token_buf)); \
+       tokens[num_tokens-1].beg = p_beg; \
+       tokens[num_tokens-1].end = p_end; \
    } STMT_END
 
 #define FREE_TOKENS \
    STMT_START { \
-     if (tokens != token_buf) \
-       Safefree(tokens); \
+       if (tokens != token_buf) \
+          Safefree(tokens); \
    } STMT_END
 
 static void
 tokens_grow(token_pos_t **token_ptr, int *token_lim_ptr, bool tokens_on_heap)
 {
-  int new_lim = *token_lim_ptr;
-  if (new_lim < 4)
-    new_lim = 4;
-  new_lim *= 2;
+    int new_lim = *token_lim_ptr;
+    if (new_lim < 4)
+	new_lim = 4;
+    new_lim *= 2;
 
-  if (tokens_on_heap) {
-    Renew(*token_ptr, new_lim, token_pos_t);
-  }
-  else {
-    token_pos_t *new_tokens;
-    int i;
-    New(57, new_tokens, new_lim, token_pos_t);
-    for (i = 0; i < *token_lim_ptr; i++)
-      new_tokens[i] = (*token_ptr)[i];
-    *token_ptr = new_tokens;
-  }
-  *token_lim_ptr = new_lim;
+    if (tokens_on_heap) {
+	Renew(*token_ptr, new_lim, token_pos_t);
+    }
+    else {
+	token_pos_t *new_tokens;
+	int i;
+	New(57, new_tokens, new_lim, token_pos_t);
+	for (i = 0; i < *token_lim_ptr; i++)
+	    new_tokens[i] = (*token_ptr)[i];
+	*token_ptr = new_tokens;
+    }
+    *token_lim_ptr = new_lim;
 }
